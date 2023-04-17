3f2b014fdcac41d85e5d5ad6f565ec5ad74705f4
Adam Jankowski <ghost17814@gmail.com>
2023-04-17 13:10:44 -0400

    GettinThere

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,6 +6,9 @@
         {
             var answer = Methods.Add(2,2, true);
             Console.WriteLine(answer);
+            Console.WriteLine(Methods.Add(0,1,true));
+            Console.WriteLine(Methods.Add(10, 10, false));  
+             
         }
     }
 }
